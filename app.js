85b299abbbc169c62f8f4b1bc95bcae0ceb18b1b
Kingsley Ibe <kingsley.ibe@decagon.dev>
2023-10-10 12:11:48 +0100

    Updated my port

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import {validateEmail} from './helpers.js'
 
 const app = express();
 
-const port = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000
 
 app.use(cors())
 app.use(express.json())
@@ -64,8 +64,8 @@ app.post('/signup', async (req, res) => {
 });
 
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 
