248728f3f529e037c95df8096b58008b9c1c20a0
Kingsley Ibe <kingsley.ibe@decagon.dev>
2023-10-14 06:48:08 +0100

    Updated index.js

--- api/appCode.js
+++ api/appCode.js
@@ -1,16 +1,65 @@
 import express from 'express';
 const router =  express.Router()
+import fs from 'fs';
+import {validateEmail} from '../helpers.js'
 
 
-router.get("/", async (req, res)=> {
-    try{
-        res.status(200).json({message:`routed successfully`})
+// router.get("/", async (req, res)=> {
+//     try{
+//         res.status(200).json({message:`routed successfully`})
 
-    }catch(error){console.log(error);
-    res.status(500).json({message:`Error!`})
+//     }catch(error){console.log(error);
+//     res.status(500).json({message:`Error!`})
+//     }
+
+// })
+
+router.post('/signup', async (req, res) => {
+  try {
+    const { email, name, message } = req.body;
+
+    if (!validateEmail(email)) {
+      return res.status(400).json({ message: 'Invalid email address' });
     }
 
-})
+    if (!name) {
+      return res.status(400).json({ message: 'Name cannot be empty' });
+    }
+
+    if (!message) {
+      return res.status(400).json({ message: 'Message cannot be empty' });
+    }
+
+    let userData = [];
+
+    if (fs.existsSync('data.json')) {
+      try {
+        const data = await fs.promises.readFile('data.json', 'utf8');
+        userData = JSON.parse(data);
+      } catch (err) {
+        console.error('Error reading data.json:', err);
+      }
+    }
+
+    const existingMessage = userData.find((user) => user.message === message && user.email === email);
+    if (existingMessage) {
+      return res.status(400).json({ message: 'It seems like you have sent the same message again. We received your initial message.' });
+    }
+
+    userData.push({ email, name, message });
+
+    try {
+      await fs.promises.writeFile('data.json', JSON.stringify(userData, null, 2));
+    } catch (err) {
+      console.error('Error writing data.json:', err);
+    }
+
+    return res.status(200).json({ message: 'Your message has been sent successfully, and I will get back to you shortly.', userData });
+  } catch (err) {
+    console.error('Error during sending message:', err);
+    return res.status(500).json({ message: 'An error occurred during sending message' });
+  }
+});
 
 
 export default router
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import fs from 'fs';
-import {validateEmail} from './helpers.js'
+// import fs from 'fs';
+// import {validateEmail} from './helpers.js'
 import 'dotenv/config'
 import appCode from "./api/appCode.js"
 
